e586048a3d4f15f67308efa19baf2cc64bb4e4e1
Pavan-Patil-22 <pavanpatil2204@gmail.com>
2026-08-09 23:19:58 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:3001", "http://127.0.0.1:3000"],
+  origin: ["http://localhost:3000", "http://localhost:3001", "https://swarnikajewellery.vercel.app","swarnikajewellery.vercel.app"],
   credentials: true
 }));
 
